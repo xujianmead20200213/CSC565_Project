08c162bf30226eca1ab0be6ea092194227af9ade
xujianmead20200213 <471103891@qq.com>
2024-04-03 01:38:54 -0400

    20240403

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ if_count = 0
 while_count = 0
 relational_operators = {'<', '<=', '>', '>=', '==', '!='}
 operators = ['+', '-', '*', '/']
-variable = {}
+variable = {'a', 'b', 'c', 'x', 'y', 'z'}
 memory = [0] * 1024  # 1kB byte-addressable memory
 # Define mappings for variables, registers, and flags along with their corresponding addresses
 mapping = {
@@ -109,13 +109,22 @@ ymc_to_machine_code = {
     'jg': '76',
     'call': '90'
 }
+convert_hlc_ymc = []
+# # Start with 0. Mark it as 1 if found while. Until finished mark it as 0
+# loop_flag = 0
+# Start with 0. Mark it as 1 if found while. Until finished mark it as 0
+if_else_flag = 0
+jmp_address = 0
+counter = 0
 
 
-def check_variables(variable):
+def check_variables(variable_code):
     # Look up the address for the variable
-    address = mapping.get(variable, 'Unknown')  # Return 'Unknown' if the variable does not exist
+    address = mapping.get(variable_code, 'Unknown')  # Return 'Unknown' if the variable does not exist
     if address == 'Unknown':
-        print("Error: " + variable + " is not defined!")
+        print("Error: " + variable_code + " is not defined!")
+    else:
+        return address
 
 
 def check_ymc_code(ymc_code):
@@ -123,10 +132,13 @@ def check_ymc_code(ymc_code):
     machine_codes = ymc_to_machine_code.get(ymc_code, 'Unknown')  # Return 'Unknown' if machine code is not found
     if machine_codes == 'Unknown':
         print("Error: " + ymc_code + " is not defined!")
+    else:
+        return machine_codes
 
 
-def parse_hlc_code(hlc):
-    for line in hlc.split('\n'):
+def parse_hlc_code(hlc_code):
+    global if_else_flag
+    for line in hlc_code.split('\n'):
         line = line.strip()
         if not line:
             continue
@@ -134,7 +146,7 @@ def parse_hlc_code(hlc):
             _, var_list = line.split(' ', 1)
             var_list = var_list.split()
             if len(var_list) > 3:
-                print("Error: unsigned variables more than 3, please leave 3 variables in this line!")
+                print("Error: Unsigned variables more than 3, please leave 3 variables in this line!")
                 sys.exit()
             for var_name in var_list:
                 unsigned_array[var_name] = ctypes.c_uint8(0)
@@ -142,21 +154,40 @@ def parse_hlc_code(hlc):
             _, var_list = line.split(' ', 1)
             var_list = var_list.split()
             if len(var_list) > 3:
-                print("Error: signed variables more than 3, please leave 3 variables in this line!")
+                print("Error: Signed variables more than 3, please leave 3 variables in this line!")
                 sys.exit()
             for var_name in var_list:
                 signed_array[var_name] = ctypes.c_int8(0)
         elif line.startswith('if'):
+            if if_else_flag == 1:
+                print("Error: If-else statement is incorrect!")
+                sys.exit()
+            if_else_flag = 1
             line_if = line.split()
             if len(line_if) == 4 and line_if[3] in relational_operators:
-                continue
+                # Todo Add here Convert HLC to YMC
+                convert_hlc_ymc.append("add machine code here mark jump address here && change counter")
             else:
                 print("Error: Only one relational operator is allowed in if statements.")
+                sys.exit()
+        elif line.startswith('else'):
+            if if_else_flag == 0:
+                print("Error: If-else statement is incorrect!")
+                sys.exit()
+            if_else_flag = 0
+            line_else = line.split()
+            if len(line_else) == 1:
+                # Todo Add here Convert HLC to YMC
+                convert_hlc_ymc.append("add machine code here fill the jump address here"
+                                       " with the location + 1 && change counter")
+            else:
+                print("Error: Else statement is incorrect!")
                 sys.exit()
         elif line.startswith('while'):
             line_while = line.split()
             if len(line_while) == 4 and line_while[3] in relational_operators:
-                continue
+                # Todo Add here Convert HLC to YMC
+                convert_hlc_ymc.append("add machine code here fill the jump address here && change counter")
             else:
                 print("Error: Only one relational operator is allowed in if statements.")
                 sys.exit()
@@ -164,21 +195,57 @@ def parse_hlc_code(hlc):
             var, expr = line.split('=')
             var = var.strip()
             expr = expr.strip()
-            check_formula(expr)
+            right_side = check_formula(expr)
             left_type = variable.get(var)
             if left_type is not None:
-                right_type = variable.get(expr)
-                if right_type is None:
-                    continue
-                if left_type != right_type:
-                    print(f"Error: Inconsistent variable types for '{var}' and '{expr}'")
+                if len(right_side) > 0:
+                    if len(right_side) == 1:
+                        right_type = variable.get(right_side[0])
+                        if right_type is not None and left_type != right_type:
+                            print(f"Error: Inconsistent variable types for '{var}' and '{right_side[0]}'")
+                            sys.exit()
+                        else:
+                            # Todo Add here
+                            convert_hlc_ymc.append("add machine code here")
+                    elif len(right_side) == 2 or len(right_side) == 4 or len(right_side) > 5:
+                        print(f"Error: The formular is incorrect!")
+                        sys.exit()
+                    elif len(right_side) == 3:
+                        right_type_1 = variable.get(right_side[0])
+                        right_type_2 = variable.get(right_side[2])
+                        if right_side[1] not in operators:
+                            print(f"Error: The formular is incorrect!")
+                            sys.exit()
+                        elif ((right_type_1 is not None and left_type != right_type_1)
+                              or (right_type_2 is not None and left_type != right_type_2)):
+                            print(f"Error: Inconsistent variable types for"
+                                  f" '{var}' and '{right_side[0]}' or '{right_side[2]}'")
+                            sys.exit()
+                        else:
+                            # Todo Add here
+                            convert_hlc_ymc.append("add machine code here")
+                    elif len(right_side) == 5:
+                        right_type_1 = variable.get(right_side[0])
+                        right_type_2 = variable.get(right_side[2])
+                        right_type_3 = variable.get(right_side[4])
+                        if right_side[1] not in operators or right_side[3] not in operators:
+                            print(f"Error: The formular is incorrect!")
+                            sys.exit()
+                        elif ((right_type_1 is not None and left_type != right_type_1)
+                              or (right_type_2 is not None and left_type != right_type_2)
+                              or (right_type_3 is not None and left_type != right_type_3)):
+                            print(f"Error: Inconsistent variable types for"
+                                  f" '{var}' and '{right_side[0]}' or '{right_side[2]}' or '{right_side[4]}'")
+                            sys.exit()
+                        else:
+                            # Todo Add here
+                            convert_hlc_ymc.append("add machine code here")
+                else:
+                    print(f"Error: Nothing on the right side!")
                     sys.exit()
             else:
-                variable[var] = expr
-            instructions.append(('assign', var, expr))
-
-        print("Instructions:", instructions)
-    return variable, instructions
+                print(f"Error: The left side of this formular is incorrect!")
+                sys.exit()
 
 
 def check_formula(expr):
@@ -186,6 +253,8 @@ def check_formula(expr):
     if operator_count > 2:
         print("Error: The formula has more than 2 operators.")
         sys.exit()
+    else:
+        return expr.split()
 
 
 def generate_assembly_code(variables, instructions):
@@ -249,6 +318,6 @@ def generate_assembly_code(variables, instructions):
     return asm.get_machine_code()
 
 
-variables, instructions = parse_hlc_code(hlc_code)
+variables, instructions = parse_hlc_code(hlc)
 machine_code = generate_assembly_code(variables, instructions)
 print('Generated Assembly Code:', machine_code)
